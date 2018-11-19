63f3860c24e27c7d3a21953ca7d40b07b521a05c
Luzhen Huang <onespark@bu.edu>
2018-11-18 19:01:45 -0500

    Initial Commit

---
+++ Stare at Patty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stare at Patty
+//
+//  Created by Spark Da Capo on 11/18/18.
+//  Copyright © 2018 OneSpark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
